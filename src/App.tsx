cbbd62aec9c79aecfdc90367be3440c2e245e959
Foxfianar <24454311+Foxfianar@users.noreply.github.com>
2024-10-31 13:48:25 +0400

    ✨ Add Adding New List or Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AddNewItem } from "./AddNewItem"
 import Column from "./Column"
 import {AppContainer} from "./styles"
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <AppContainer>
       <Column text="Todo: "/>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
     </AppContainer>
   )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,3 +1,4 @@
+import { AddNewItem } from './AddNewItem';
 import { Card } from './Card';
 import { ColumnContainer, ColumnTitle } from './styles'
 
@@ -12,6 +13,12 @@ export default function Column({text}: ColumnType) {
        <Card text="Generate app scaffold" />
        <Card text="Learn TypeScript" />
        <Card text="Begin to use static typing" />
+      <AddNewItem
+         toggleButtonText="+ Add another card"
+         onAdd={console.log}
+         dark
+       />
+
      </ColumnContainer>
    )
 }
